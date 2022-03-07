2c602de49b830c992b3e9f772242afd04c7c4748
Hugo Tamayo <doctamayot@hotmail.com>
2022-03-06 21:55:00 -0500

    first commit

--- pages/api/users/forgot.js
+++ pages/api/users/forgot.js
@@ -40,7 +40,10 @@ handler.post(async (req, res) => {
 
     //forgotPassword(email, token);
     const sgMail = require('@sendgrid/mail');
-    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+    //sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+    sgMail.setApiKey(
+      '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'
+    );
     const msg = {
       to: { email },
       from: 'universalactinginfo@gmail.com',
