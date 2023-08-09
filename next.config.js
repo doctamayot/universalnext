ae162fefec9ca5a79d2d4376c1bc03179e26d885
Hugo Tamayo <doctamayot@hotmail.com>
2023-08-08 21:11:54 -0500

    quitar getserver2

--- next.config.js
+++ next.config.js
@@ -1,12 +1,38 @@
-const withTM = require('next-transpile-modules')([
-  '@square/web-sdk',
-  'react-square-web-payments-sdk',
+const withTM = require("next-transpile-modules")([
+  "@square/web-sdk",
+  "react-square-web-payments-sdk",
 ]);
 
 module.exports = withTM({
   reactStrictMode: false,
-  images: { domains: ['res.cloudinary.com'] },
+  images: { domains: ["res.cloudinary.com"] },
   experimental: {
-    esmExternal: 'loose',
+    esmExternal: "loose",
   },
 });
+
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,DELETE,PATCH,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
