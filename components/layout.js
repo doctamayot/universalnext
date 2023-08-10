83f99f789209d9c935df199289036288a1d343c6
Hugo Tamayo <doctamayot@hotmail.com>
2023-08-10 15:23:48 -0500

    darkmode

--- components/layout.js
+++ components/layout.js
@@ -2,17 +2,17 @@ import Navigation from "../components/Navigation";
 // import styles from "../styles/sass/main.module.scss";
 // import Switch from "react-input-switch";
 
-import { useContext } from "react";
-import { Store } from "../utils/Store.js";
+// import { useContext } from "react";
+// import { Store } from "../utils/Store.js";
 import Footer from "./Footer";
 
 const Layout = ({ children }) => {
-  const { state, dispatch } = useContext(Store);
-  const { darkMode } = state;
+  // const { state } = useContext(Store);
+  // const { darkMode } = state;
 
-  const darkModeChangeHandler = () => {
-    dispatch({ type: darkMode ? "DARK_MODE_OFF" : "DARK_MODE_ON" });
-  };
+  // const darkModeChangeHandler = () => {
+  //   dispatch({ type: darkMode ? "DARK_MODE_OFF" : "DARK_MODE_ON" });
+  // };
 
   return (
     <div className={darkMode ? "oscuro" : ""}>
